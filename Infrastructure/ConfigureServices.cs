d5f0a9fd4605ac21be5fa1b44054ddc761cf9986
asen23 <andersenlim23@gmail.com>
2022-06-01 23:02:13 +0700

    configure DI service

---
+++ Infrastructure/ConfigureServices.cs
@@ -0,0 +1,17 @@
+﻿using Application.Common.Interfaces;
+using Infrastructure.Persistence;
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+    {
+        services.AddDbContext<ApplicationDbContext>();
+        
+        services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
+
+        return services;
+    }
+}
