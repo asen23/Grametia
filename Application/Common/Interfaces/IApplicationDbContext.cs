8b4c419e053aea370e4077d37a8866ffac1b8e57
asen23 <andersenlim23@gmail.com>
2022-06-01 12:40:16 +0700

    add dbcontext

---
+++ Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿#region
+
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+#endregion
+
+namespace Application.Common.Interfaces;
+
+public interface IApplicationDbContext
+{
+    public DbSet<Book> Books { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Transaction> Transactions { get; set; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
---
+++ Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence;
+
+public class ApplicationDbContext : DbContext, IApplicationDbContext
+{
+    public DbSet<Book> Books { get; set; } = default!;
+    public DbSet<User> Users { get; set; } = default!;
+    public DbSet<Transaction> Transactions { get; set; } = default!;
+
+    private string DbPath { get; }
+
+    public ApplicationDbContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = Path.Join(path, "database.db");
+    }
+
+    // The following configures EF to create a Sqlite database file in the
+    // special "local" folder for your platform.
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>()
+            .HasOne(u => u.Cart)
+            .WithOne(c => c.User)
+            .HasForeignKey<Cart>(c => c.UserId);
+    }
+}
