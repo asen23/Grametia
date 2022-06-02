a34fe75d2e8bbee6032b1617ca58408ae1b7069a
asen23 <andersenlim23@gmail.com>
2022-06-02 21:05:23 +0700

    update dbcontext

--- Infrastructure/Persistence/ApplicationDbContext.cs
+++ Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,17 +1,15 @@
-﻿using Application.Common.Interfaces;
+﻿#region
+
+using Application.Common.Interfaces;
 using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+
+#endregion
 
 namespace Infrastructure.Persistence;
 
 public class ApplicationDbContext : DbContext, IApplicationDbContext
 {
-    public DbSet<Book> Books { get; set; } = default!;
-    public DbSet<User> Users { get; set; } = default!;
-    public DbSet<Transaction> Transactions { get; set; } = default!;
-
-    private string DbPath { get; }
-
     public ApplicationDbContext()
     {
         var folder = Environment.SpecialFolder.LocalApplicationData;
@@ -19,10 +17,17 @@ public class ApplicationDbContext : DbContext, IApplicationDbContext
         DbPath = Path.Join(path, "database.db");
     }
 
+    private string DbPath { get; }
+    public DbSet<Book> Books { get; set; } = default!;
+    public DbSet<User> Users { get; set; } = default!;
+    public DbSet<Transaction> Transactions { get; set; } = default!;
+
     // The following configures EF to create a Sqlite database file in the
     // special "local" folder for your platform.
     protected override void OnConfiguring(DbContextOptionsBuilder options)
-        => options.UseSqlite($"Data Source={DbPath}");
+    {
+        options.UseSqlite($"Data Source={DbPath}");
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -30,5 +35,20 @@ public class ApplicationDbContext : DbContext, IApplicationDbContext
             .HasOne(u => u.Cart)
             .WithOne(c => c.User)
             .HasForeignKey<Cart>(c => c.UserId);
+        modelBuilder.Entity<Transaction>()
+            .HasOne(t => t.Detail)
+            .WithOne(d => d.Transaction)
+            .HasForeignKey<Detail>(d => d.TransactionId);
+        modelBuilder.Entity<User>()
+            .HasData(new User
+            {
+                Id = 1,
+                Username = "Admin",
+                Address = "Admin Street",
+                Email = "admin@admin.com",
+                Password = "root",
+                PhoneNumber = "08123456789",
+                Role = "admin"
+            });
     }
 }
