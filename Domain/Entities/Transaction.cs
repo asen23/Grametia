73375f44a740a7ea3bbd76ee2dc1eb46e2cbd68b
asen23 <andersenlim23@gmail.com>
2022-06-03 22:27:47 +0700

    update domain

--- Domain/Entities/Transaction.cs
+++ Domain/Entities/Transaction.cs
@@ -3,6 +3,8 @@
 public class Transaction : BaseEntity
 {
     public DateTime DateTime { get; set; } = default!;
+    public string PaymentMethod { get; set; } = default!;
+    public string Courier { get; set; } = default!;
 
     public User User { get; set; } = default!;
     public Detail Detail { get; set; } = default!;
--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -2,6 +2,11 @@
 
 public class User : BaseEntity
 {
+    public User()
+    {
+        Cart = new Cart();
+    }
+
     public string Username { get; set; } = default!;
     public string Address { get; set; } = default!;
     public string PhoneNumber { get; set; } = default!;
@@ -9,5 +14,5 @@ public class User : BaseEntity
     public string Password { get; set; } = default!;
     public string Role { get; set; } = default!;
 
-    public Cart Cart { get; set; } = default!;
+    public Cart Cart { get; set; }
 }
