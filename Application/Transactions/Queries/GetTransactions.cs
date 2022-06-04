e9e643d77f631e25f1bc40c0190a7b475d4f0420
asen23 <andersenlim23@gmail.com>
2022-06-04 17:17:03 +0700

    add get list query

---
+++ Application/Transactions/Queries/GetTransactions.cs
@@ -0,0 +1,33 @@
+﻿#region
+
+using Application.Common.Interfaces;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+#endregion
+
+namespace Application.Transactions.Queries;
+
+public record GetTransactions : IRequest<List<Transaction>>
+{
+}
+
+public class GetTransactionsQueryHandler : RequestHandler<GetTransactions, List<Transaction>>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetTransactionsQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    protected override List<Transaction> Handle(GetTransactions request)
+    {
+        return _context.Transactions
+            .Include(t => t.Detail.Items)
+            .ThenInclude(di => di.Book)
+            .Include(t => t.User)
+            .ToList();
+    }
+}
---
+++ Application/Users/Queries/GetUsers.cs
@@ -0,0 +1,28 @@
+﻿#region
+
+using Application.Common.Interfaces;
+using Domain.Entities;
+using MediatR;
+
+#endregion
+
+namespace Application.Users.Queries;
+
+public record GetUsers : IRequest<List<User>>
+{
+}
+
+public class GetUsersQueryHandler : RequestHandler<GetUsers, List<User>>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetUsersQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    protected override List<User> Handle(GetUsers request)
+    {
+        return _context.Users.ToList();
+    }
+}
