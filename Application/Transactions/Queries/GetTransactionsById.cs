c3ef6d2c9ed057978a090b8fa3241637cda93a19
asen23 <andersenlim23@gmail.com>
2022-06-04 14:00:43 +0700

    add transaction query

---
+++ Application/Transactions/Queries/GetTransactionsById.cs
@@ -0,0 +1,28 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Transactions.Queries;
+
+public record GetTransactionsById : IRequest<List<Transaction>>
+{
+    public long Id { get; set; } = default!;
+}
+
+public class GetTransactionsByIdQueryHandler : IRequestHandler<GetTransactionsById, List<Transaction>>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetTransactionsByIdQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Transaction>> Handle(GetTransactionsById request, CancellationToken cancellationToken)
+    {
+        return await _context.Transactions
+            .Where(t => t.Id == request.Id)
+            .ToListAsync(cancellationToken);
+    }
+}
---
+++ Application/Transactions/Queries/GetTransactionsByUserId.cs
@@ -0,0 +1,28 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Transactions.Queries;
+
+public record GetTransactionsByUserId : IRequest<List<Transaction>>, IAuthorizeable
+{
+    public long UserId { get; set; } = default!;
+}
+
+public class GetTransactionsByUserIdQueryHandler : IRequestHandler<GetTransactionsByUserId, List<Transaction>>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetTransactionsByUserIdQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Transaction>> Handle(GetTransactionsByUserId request, CancellationToken cancellationToken)
+    {
+        return await _context.Transactions
+            .Where(t => t.User.Id == request.UserId)
+            .ToListAsync(cancellationToken);
+    }
+}
