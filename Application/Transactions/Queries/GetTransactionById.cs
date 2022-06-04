0b2336aa3e82107605edec119da6f7752215bfd9
asen23 <andersenlim23@gmail.com>
2022-06-04 17:16:14 +0700

    rename query to reflect better

---
+++ Application/Transactions/Queries/GetTransactionById.cs
@@ -0,0 +1,36 @@
+﻿#region
+
+using Application.Common.Interfaces;
+using Application.Common.Models;
+using Domain.Entities;
+using MediatR;
+
+#endregion
+
+namespace Application.Transactions.Queries;
+
+public record GetTransactionsById : IRequest<ValidateableResponse<Transaction>>
+{
+    public long Id { get; set; } = default!;
+}
+
+public class GetTransactionsByIdQueryHandler : IRequestHandler<GetTransactionsById, ValidateableResponse<Transaction>>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetTransactionsByIdQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<ValidateableResponse<Transaction>> Handle(GetTransactionsById request,
+        CancellationToken cancellationToken)
+    {
+        var transaction = await _context.Transactions
+            .FindAsync(new object[] { request.Id }, cancellationToken);
+
+        return transaction == null
+            ? new ValidateableResponse<Transaction>(null!, "Transaction does not exist")
+            : new ValidateableResponse<Transaction>(transaction);
+    }
+}
