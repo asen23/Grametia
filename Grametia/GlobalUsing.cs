69f8eecca08618c40888d771bba05faa0e839c58
asen23 <andersenlim23@gmail.com>
2022-06-04 20:33:25 +0700

    add init

---
+++ Grametia/GlobalUsing.cs
@@ -0,0 +1,5 @@
+﻿#region
+
+global using Grametia.Utility;
+
+#endregion
--- Grametia/Program.cs
+++ Grametia/Program.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿#region
+
+using Application.Common.Interfaces;
+using Grametia.Menu;
+using Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+#endregion
 
 Console.WriteLine("Hello, World!");
+var app = Host.CreateDefaultBuilder()
+    .ConfigureServices((_, services) =>
+        services
+            .AddApplicationServices()
+            .AddInfrastructureServices()
+    )
+    .Build();
+
+RunApp(app.Services);
+await app.StartAsync();
+
+static async void RunApp(IServiceProvider services)
+{
+    var scope = services.CreateScope();
+    var provider = scope.ServiceProvider;
+
+    var db = provider.GetRequiredService<ApplicationDbContext>();
+    await db.Database.MigrateAsync();
+    
+    Seed.Initialize(provider.GetRequiredService<IApplicationDbContext>(), CancellationToken.None);
+
+    var mediator = provider.GetRequiredService<ISender>();
+
+    await new MainMenu(mediator).Run();
+}
